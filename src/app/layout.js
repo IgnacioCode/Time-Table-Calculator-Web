4cca1fc6a001c630c6371617f2cf0c2498957d85
Ignacio Romero <nachoromero84@hotmail.com>
2025-12-16 02:48:55 -0300

    Se agrega modulo analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from 'next-themes';
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class">
+          <Analytics />
           {children}
         </ThemeProvider>
       </body>
